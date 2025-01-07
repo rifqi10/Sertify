1fcf073ce8cfead13695d369491ced7e57ab05a2
rifqi10 <53550730+rifqi10@users.noreply.github.com>
2025-01-07 13:47:40 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.ejs',
+    './public/styles/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
